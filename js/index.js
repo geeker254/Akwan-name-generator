ec5194b1f5665e4a6c0c0e4a668cad75c235ed74
geeker254 <eugenebiwott@gmail.com>
2020-07-22 16:04:22 +0300

    IF statement names

---
+++ js/index.js
@@ -0,0 +1,71 @@
+document.getElementById(Bdate);
+document.getElementById(Gender);
+
+
+//FORMULA
+if (Gender=Male) {
+    console.log(Male);
+}
+    else {
+        console.log(Female);
+    }
+
+
+let male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+let female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
+if (Male=Male(0)) {
+    window.alert("Kwasi");
+
+    if (Male=Male(1)) {
+        window.alert("Kwadwo");
+
+        if (Male=Male(2)) {
+            window.alert("Kwabena");
+
+            if (Male=Male(3)) {
+                window.alert("Kwaku");
+
+                if (Male=Male(4)) {
+                    window.alert("Yaw");
+
+                    if(Male=Male(5)) {
+                        window.alert("Kofi");
+
+                        if (Male=Male(6)) {
+                            window.alert("Kwame")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+if (Female=Female(0)) {
+    window.alert("Akosua");
+
+    if (Female=female(1)) {
+        window.alert("Adwoa");
+
+        if (Female=Female(2)) {
+            window.alert("Abenaa");
+
+            if (Female=Female(3)) {
+                window.alert("Akua");
+
+                if (Female=Female(4)) {
+                    window.alert("Yaa");
+
+                    if(Female=Female(5)) {
+                        window.alert("Afua");
+
+                        if (Female=Female(6)) {
+                            window.alert("Ama");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
