aef136ed622ceccf92d0100dd126999716263fa9
geeker254 <eugenebiwott@gmail.com>
2020-07-22 19:01:28 +0300

    Formulas

--- js/index.js
+++ js/index.js
@@ -1,71 +1,111 @@
-document.getElementById(Bdate);
-document.getElementById(Gender);
+form.submit(Gender);
+form.submit(Bdate);
 
+let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-//FORMULA
-if (Gender=Male) {
-    console.log(Male);
-}
-    else {
-        console.log(Female);
+let weekday = Bdate;
+let MM = mm;
+let DD = dd;
+let CCYY = yyyy
+weekday(d) = (((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
+if (weekday = week(0)) {
+    weekday = "Sunday";
+    if (weekday = week(1)) {
+        weekday = "Monday";
+        if (weekday = week(2)) {
+            weekday = "Tuesday";
+            if (weekday = week(3)) {
+                weekday = "Wednesday";
+                if (weekday = week(4)) {
+                    weekday = "Thursday";
+                    if (weekday = week(5)) {
+                        weekday = "Friday";
+                        if (weekday = week(6)) {
+                            weekday = "Saturday";
+                        }
+                    }
+                }
+            }
+        }
     }
+    alert.weekday(d);
+}
 
 
 let male = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
 let female = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 
-if (Male=Male(0)) {
-    window.alert("Kwasi");
+var maledays = week.concat(male);
+var femaledays = week.concat(female);
 
-    if (Male=Male(1)) {
-        window.alert("Kwadwo");
 
-        if (Male=Male(2)) {
-            window.alert("Kwabena");
-
-            if (Male=Male(3)) {
-                window.alert("Kwaku");
-
-                if (Male=Male(4)) {
-                    window.alert("Yaw");
-
-                    if(Male=Male(5)) {
-                        window.alert("Kofi");
-
-                        if (Male=Male(6)) {
-                            window.alert("Kwame")
+if (Gender=male) {
+    if (Male=Male(0)) {
+        window.alert("Kwasi");
+    
+        if (Male=Male(1)) {
+            window.alert("Kwadwo");
+    
+            if (Male=Male(2)) {
+                window.alert("Kwabena");
+    
+                if (Male=Male(3)) {
+                    window.alert("Kwaku");
+    
+                    if (Male=Male(4)) {
+                        window.alert("Yaw");
+    
+                        if(Male=Male(5)) {
+                            window.alert("Kofi");
+    
+                            if (Male=Male(6)) {
+                                window.alert("Kwame")
+                            }
                         }
                     }
                 }
             }
         }
     }
-}
 
-if (Female=Female(0)) {
-    window.alert("Akosua");
-
-    if (Female=female(1)) {
-        window.alert("Adwoa");
-
-        if (Female=Female(2)) {
-            window.alert("Abenaa");
-
-            if (Female=Female(3)) {
-                window.alert("Akua");
-
-                if (Female=Female(4)) {
-                    window.alert("Yaa");
-
-                    if(Female=Female(5)) {
-                        window.alert("Afua");
-
-                        if (Female=Female(6)) {
-                            window.alert("Ama");
+    else (Gender = Female); {
+        if (Female=Female(0)) {
+            window.alert("Akosua");
+        
+            if (Female=female(1)) {
+                window.alert("Adwoa");
+        
+                if (Female=Female(2)) {
+                    window.alert("Abenaa");
+        
+                    if (Female=Female(3)) {
+                        window.alert("Akua");
+        
+                        if (Female=Female(4)) {
+                            window.alert("Yaa");
+        
+                            if(Female=Female(5)) {
+                                window.alert("Afua");
+        
+                                if (Female=Female(6)) {
+                                    window.alert("Ama");
+                                }
+                            }
                         }
                     }
                 }
             }
         }
-    }
+        
 }
+}
+
+
+
+
+
+
+function newFunction() {
+    app.alert("Day of week: " + util.printd("dddd", oDate));
+}
+
